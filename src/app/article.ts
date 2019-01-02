eb277d09a5f4960822f8231d53f47e79dea6db7f
Kurt Rogiers <kurtrogiers@MacBook-Pro-van-Kurt.local>
2019-01-02 15:35:02 +0100

    first commit

---
+++ src/app/article.ts
@@ -0,0 +1,7 @@
+export interface Article {
+    product : String;
+    amount : Number;
+    unitPrice : Number;
+    total : Number;
+    articleId : String;
+}
---
+++ src/app/mock_articles.ts
@@ -0,0 +1,7 @@
+import { Article } from './article';
+
+export const ARTICLES: Article[] = [
+    { product: 'Apple', amount: 2, unitPrice: 2, total: 4, articleId: '1' },
+    { product: 'Cake', amount: 1, unitPrice: 3, total: 3, articleId: '2' },
+    { product: 'Boni Chocolat', amount: 3, unitPrice: 3, total: 9, articleId: '3' }
+];
